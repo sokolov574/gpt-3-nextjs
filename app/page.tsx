32d3a1dd4b14be4098a575382b6b47caa13dfc45
sokolov574 <sokolovloveoleg@gmail.com>
2024-05-01 16:12:32 +0200

    added some style

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,27 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "@/styles/Home.module.css";
-import mainImage from '@/assets/images/main-image.png'
+import mainImage from '@/assets/images/main-image.png';
+
 
 export default function Home() {
   return (
     <>
-    <Head>
-        <title>Cringe AI - Create cringy motivational quotes</title>
-        <meta name="description" content="by Coding in Flow" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
     <main>
-    <h1>Cringe AI</h1>
-        <h2>powered by GPT-3</h2>
-        <div>Enter a topic and the AI will generate a super cringy motivational quote</div>
-        <div className={styles.mainImageContainer}>
-          <Image
+   {/*  <h1>Cringe AI</h1>
+    <h2>powered by GPT-3</h2>
+    <div>Enter a topic and the AI will generate a super cringy motivational quote</div> */}
+
+    <div className={styles.mainImageContainer}>
+        <Image
             src={mainImage}
             fill
             alt='A picture of a woman holding both her hands in front of her face'
             priority
             className={styles.mainImage}
-          />
-        </div>
-      </main></>
+        />
+    </div>
+    </main>
+      </>
   );
 }
