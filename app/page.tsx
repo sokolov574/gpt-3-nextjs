4cb8c155ffeb7611186376150f4b383e68613656
sokolov574 <sokolovloveoleg@gmail.com>
2024-05-02 12:57:45 +0200

    fix bags with style for main

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,31 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "@/styles/Home.module.css";
-import mainImage from '@/assets/images/main-image.png';
-
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '@/styles/Home.module.css'
+import mainImage from '@/assets/images/main-image.png'
+import { Form, Button, Spinner } from 'react-bootstrap'
+/* import { FormEvent, useState } from 'react' */
 
 export default function Home() {
+
+ 
+
   return (
     <>
-    <main>
-   {/*  <h1>Cringe AI</h1>
-    <h2>powered by GPT-3</h2>
-    <div>Enter a topic and the AI will generate a super cringy motivational quote</div> */}
-
-    <div className={styles.mainImageContainer}>
-        <Image
+      <main className={styles.main}>
+        <h1>Cringe AI</h1>
+        <h2>powered by GPT-3</h2>
+        <div>Enter a topic and the AI will generate a super cringy motivational quote</div>
+        <div className={styles.mainImageContainer}>
+          <Image
             src={mainImage}
             fill
             alt='A picture of a woman holding both her hands in front of her face'
             priority
             className={styles.mainImage}
-        />
-    </div>
-    </main>
-      </>
-  );
+          />
+        </div>
+      
+      </main>
+    </>
+  )
 }
