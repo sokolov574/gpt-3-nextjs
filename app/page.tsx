535f3195c7653bb08e2512092ba8b44e0ef8554f
sokolov574 <sokolovloveoleg@gmail.com>
2024-05-04 23:11:27 +0200

    Add form handling and API request function

--- app/page.tsx
+++ app/page.tsx
@@ -11,11 +11,31 @@ import { FormEvent, useState } from 'react'
 
 export default function Home() {
 
-  const [quote, setQuote] = useState('');
+  const [quote, setQuote] = useState("");
   const [quoteLoading, setQuoteLoading] = useState(false);
+  const [quoteLoadingError, setQuoteLoadingError] = useState(false);
 
   async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const prompt = formData.get("prompt")?.toString().trim();
 
+    if (prompt) {
+      try {
+        setQuote("");
+        setQuoteLoadingError(false);
+        setQuoteLoading(true);
+
+        const response = await fetch("/api/cringe?prompt=" + encodeURIComponent(prompt));
+        const body = await response.json();
+        setQuote(body.quote);
+      } catch (error) {
+        console.error(error);
+        setQuoteLoadingError(true);
+      } finally {
+        setQuoteLoading(false);
+      }
+    }
   }
 
   return (
